0153c4fa29c2da4ccee0655f857c5532cc82512f
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2020-10-02 17:53:30 +0300

    fix: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,15 +5,12 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample( sampleActivity ) {
 let numSampleActivity = parseFloat(sampleActivity)
-let k = 0.693 / HALF_LIFE_PERIOD
+let Tk = 0.693 / HALF_LIFE_PERIOD
 let result
-if (typeof sampleActivity !== "srting" ||  sampleActivity === undefined || numsampleActivity < 0){
-  return false
-} else {
-  result = Math.ceil(Math.log(MODERN_ACTIVITY / numSampleActivity) / k)
-
-}
+  if (typeof sampleActivity !== "srting" ||  sampleActivity === undefined || numsampleActivity < 0){
+    return false
+  } else {
+    result = Math.round(Math.log(MODERN_ACTIVITY / numSampleActivity) / Tk)
+  }
   return result
-
-  
 };
