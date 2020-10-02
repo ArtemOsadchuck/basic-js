71dfa2837388358e3f0665d398b8abfdbbbfb736
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2020-10-02 17:49:45 +0300

    feat: added functionality to carbon-dating, count-cats & dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  
+  let matrixF1 = [].concat(...matrix)
+  
+  let filterMatrix = matrixF1.filter(item => item === '^^')
+  
+  console.log(matrixF1)
+  // console.log(matrixF2)
+  // console.log(filterMatrix)
+
+  let result = `${filterMatrix.length}`
+  if (result === "0"){
+    return 0
+  }
+  return result
 };
