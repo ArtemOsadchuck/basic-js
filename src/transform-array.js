d3daaac23a176492a3de4ca442352c0d62337bcb
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2020-10-05 12:47:18 +0300

    feat: add trnsform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    if (!Array.isArray(arr)) { 
+    throw new  castomError ("error") }
+      let newArr = arr.slice()
+      let resultArr = []
+  
+      
+  
+      for (let i = 0; i < newArr.length; i++){
+    
+        if (newArr[i] === '--double-next' ){
+          newArr[i]= newArr[i + 1] 
+          if (newArr[i] === undefined){
+            newArr[i] = newArr[i-1]
+            resultArr.pop(newArr[i])
+              
+          }
+    
+        } else if (newArr[i] === `--discard-next`){
+          i++
+          i++
+          if (newArr[i] === undefined){
+            newArr[i] = newArr[i-3]
+            resultArr.pop(newArr[i])
+              
+          }
+  
+        } else if (newArr[i] === `--discard-prev`){
+          resultArr.pop(newArr[i])
+          i++
+    
+        } else if (newArr[i] === `--double-prev`){
+          
+          newArr[i] = newArr[i - 1]
+          if( newArr[i] === undefined){
+            resultArr.pop(newArr[i])
+            i++       
+          }
+        }
+    
+    
+      resultArr.push(newArr[i])
+    
+      } 
+    
+
+
+
+      for (let i = 0; i < resultArr.length; i++){
+        if (resultArr[i] === '--double-prev' || resultArr[i] === '--discard-next' || resultArr[i] === '--double-next' || resultArr[i] === '--discard-prev') {
+          resultArr.splice(i, 1)
+        }
+    }
+     return resultArr
+    
+
 };
