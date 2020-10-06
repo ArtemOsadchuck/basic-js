dfcf17b12219ab2f6d4e6a8a1c554757a7b40e83
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2020-10-06 16:04:09 +0300

    feat: added solutionn for extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+ 
+  str = String(str)
+    
+
+  let repeatTimes = options.repeatTimes
+  if (repeatTimes === undefined){
+    repeatTimes = 1
+  }
+
+  let separator = String(options.separator)
+  if (separator === "undefined"){
+    separator = "+"
+  }
+
+  let addition = String(options.addition)
+  if ( addition === "undefined"){
+    addition = ""
+  }
+
+  let additionRepeatTimes = options.additionRepeatTimes
+  if (additionRepeatTimes === undefined){
+    additionRepeatTimes = 1
+  }
+
+  let additionSeparator = String(options.additionSeparator)
+   if (additionSeparator === "undefined"){
+    additionSeparator = "|"
+   }
+
+ 
+    let addArr = []
+    let strAddArr = []
+    for (let i =0; i < additionRepeatTimes; i++){
+      addArr.push(addition)
+    }
+    addArr = addArr.join(additionSeparator)
+
+    for( let k = 0; k < repeatTimes; k++){
+      strAddArr.push(str + addArr)
+    }
+
+   return strAddArr.join(separator)
+
 };
   
